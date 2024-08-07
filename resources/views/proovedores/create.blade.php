864466e04705db0105d4c3d73df097b7b85769a2
ArmandoGutMo <argutmo819@gmail.com>
2024-08-07 10:37:09 -0500

    Add new controllers, models, migrations, and views for proveedores

---
+++ resources/views/proovedores/create.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('title', 'Proovedores')
+
+@section('content')
+
+<br>
+    <h2>Agregar un proovedor</h2>
+    <form action="/proovedores" method="POST" enctype="multipart/form-data">
+        @csrf
+        <div class="mb-3">
+          <label for="nombreproovedor" class="form-label">Nombre del proovedor</label>
+          <input type="text" class="form-control" id="nombre" name="nombre">
+        </div>
+        <div class="mb-3">
+            <label for="nit" class="form-label">Nit</label>
+            <input type="text" class="form-control" id="nit" name="nit">
+        </div>
+        <div class="mb-3">
+            <label for="telefono" class="form-label">Telefono</label>
+            <input type="text" class="form-control" id="telefono" name="telefono">
+          </div>
+          <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="text" class="form-control" id="email" name="email">
+          </div>
+        <br>
+        <br>
+        <button type="submit" class="btn btn-success">Guardar</button>
+    </form>
+
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\ClientesController;
+use App\Http\Controllers\ProovedorController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/saludo', function () {
+    return 'Nuestro clientes son nuestra prioridad';
+});
+
+Route::get('/mensaje', [ClientesController::class, 'mensaje']);
+
+Route::resource('/proovedores', ProovedorController::class);
